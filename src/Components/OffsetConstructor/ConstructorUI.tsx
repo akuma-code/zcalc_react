fd5eed8053785c098fa03b12bc8798fa4f60f6ba
akuma-code <akumapl86@gmail.com>
2023-01-18 17:15:47 +0300

    sync

--- src/Components/OffsetConstructor/ConstructorUI.tsx
+++ src/Components/OffsetConstructor/ConstructorUI.tsx
@@ -1,6 +1,9 @@
 import React, { useRef, useState, useId } from 'react'
+import { DivProps } from '../../Types'
+import ButtonFr from '../Constructor/ViewModel/UI/ButtonFr'
 import { _ID } from '../Constructor/ViewModel/ViewModelConst'
-import { ICoords, IModelSize, OffsetFrameModel } from './Model/OffsetModel'
+import { IcFrameRight, IcPlus, IcReset } from '../Icons/IconsPack'
+import { ICoords, IModelSize, OffsetCanvas, OffsetFrameModel } from './Model/OffsetModel'
 
 type Props = {}
 
@@ -8,40 +11,59 @@ export const ConstructorUI = (props: Props) => {
   const [offset, setOffset] = useState({ x: 0, y: 0 })
   const [model, setModel] = useState<OffsetFrameModel>(new OffsetFrameModel(6, 10))
   const [MStack, setMStack] = useState([] as OffsetFrameModel[])
+  const [Canvas, setCanvas] = useState(new OffsetCanvas())
 
-  function addModelRight(newmodel: OffsetFrameModel) {
-    const newOffset = { x: offset.x + newmodel.w, y: offset.y + newmodel.h }
-    setOffset(prev => ({ ...newOffset }))
-    setMStack(prev => [...prev, newmodel])
+
+  function restart() {
+    Canvas.reset()
+    Canvas.init()
+    updateCanvas()
+  }
+  function updateCanvas() {
+    setCanvas(prev => prev.getCopy())
+
+  }
+
+  function addR() {
+    Canvas.appendRight(3, 5)
+    updateCanvas()
   }
   return (
     <div className='border bg-red-200 mt-14 p-2'>
       <h1 className='text-2xl text-center cursor-pointer'
-        onClick={() => addModelRight(new OffsetFrameModel(10, 10))}
+      // onClick={() => { }}
       >
         Container
       </h1>
+      <div>
+        <ButtonFr logo={<IcPlus hw={6} />} bgColor='teal' clickFn={() => restart()} >INIT</ButtonFr>
+        <ButtonFr logo={<IcFrameRight hw={6} />} bgColor='red' clickFn={() => addR()} >AppendR</ButtonFr>
+        <ButtonFr logo={<IcReset hw={6} />} bgColor='green' clickFn={() => updateCanvas()} >Reset</ButtonFr>
+      </div>
       <div className='w-[30em] h-[30em] bg-red-500  outline-double outline-4 outline-black mt-2 relative'>
         {
-          MStack.map(model =>
-            <div className={`relative left-${offset.x} top-${offset.y}`} key={model.id}
+          Canvas.VMs.map(m =>
+            <OffModel {...m} key={m.id} pos={m.pos}
+            // className={`relative left-${m.pos?.x} top-${m.pos?.y}`} key={model.id}
+            />
+          )
 
-            >{view(model)}</div>)
 
         }
+        {/* <OffModel w={3} h={4} off={{ x: 0, y: 0 }} /> */}
       </div>
     </div>
 
   )
 }
 
-function view(size: IModelSize) {
+
+const OffModel: React.FC<{ w: number, h: number, pos?: ICoords } & DivProps> = (props) => {
   const id = _ID()
-  const DIV = () => (
-    <div key={id} className={`w-[${size.w}em] h-[${size.h}em]
-    bg-green-500
-    `}
-    >w: {size.w} -- h: {size.h} -- ID: {id}
-    </div>)
-  return DIV()
+  const { w, h, pos } = props
+  return (
+    <div className={`relative  left-[${pos?.x}em] top-[${pos?.y}em] block w-fit`} key={id}  >
+      <div className={`w-[${w}em] h-[${h}em] bg-green-500 absolute outline outline-slate-500`}></div>
+    </div>
+  )
 }
--- src/Components/OffsetConstructor/Model/OffsetModel.tsx
+++ src/Components/OffsetConstructor/Model/OffsetModel.tsx
@@ -14,33 +14,66 @@ export class OffsetFrameModel {
     constructor(
         public w: number,
         public h: number,
+        public pos?: ICoords
     ) {
         this.id = _ID()
         this.w = w
         this.h = h
+        this.pos = pos || { x: 0, y: 0 }
     }
 
 }
 
 export class OffsetCanvas {
-    x0: number
-    y0: number
-    offset: ICoords
-    constructor(
-
-    ) {
-        this.x0 = 0
-        this.y0 = 0
-        this.offset = { x: 0, y: 0 }
+    VMs = [] as OffsetFrameModel[]
+    // constructor() {
+    //     this.VMs = []
+    //     console.log('viewModels', this.VMs)
+    // }
+    init() {
+        if (this.VMs.length > 0) {
+            console.log('Init failed')
+            // return this.VMs
+        }
+        console.log('init')
+        this.VMs = []
+        // const newmodel = new OffsetFrameModel(6, 10)
+        // this.VMs.push(new OffsetFrameModel(6, 10))
+        // return this.VMs
     }
 
+    appendRight(w: number, h: number) {
+        // if (this.VMs.length === 0) return this.init()
+        const pos = this.VMs.reduce((off, current) => {
+            off.x += current.w
+            off.y = 0
+            return off
+        }, { x: 0, y: 0 })
+        console.log('offset: ', pos)
+        // const newmodel = new OffsetFrameModel(w, h, pos)
+        this.VMs.push(new OffsetFrameModel(w, h, pos))
+        // return newmodel
+    }
+    appendTop(w: number, h: number) {
+        const off = this.VMs.reduce((off, current) => {
+            off.x = 0
+            off.y += current.h
+            return off
+        }, { x: 0, y: 0 })
+        this.VMs.push(new OffsetFrameModel(w, h, off))
+        return this.VMs
+    }
 
-    addFrame(frame: OffsetFrameModel) {
-        this.offset = {
-            x: this.offset.x + frame.w,
-            y: this.offset.y + frame.h
-        }
+    reset() {
+        console.log('Restarted');
+        console.log('viewModels', this.VMs)
+        this.VMs = []
+    }
 
-
+    getCopy() {
+        const newCanvas = new OffsetCanvas()
+        newCanvas.VMs = this.VMs
+        console.log('newCanvas', newCanvas)
+        return newCanvas
     }
 }
